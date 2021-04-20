2c1dcb7776bbc10acdcdb180e8acc4d92d8234b8
krishpranav <krisna.pranav@gmail.com>
2021-04-20 09:50:59 +0530

    updated impact file

--- lib/models/fidius/cve_db/impact.rb
+++ lib/models/fidius/cve_db/impact.rb
@@ -1,7 +1,3 @@
-class FIDIUS::CveDb::Cvss < FIDIUS::CveDb::CveConnection
-  attr_accessible :score, :source, :generated_on, :access_vector, :access_complexity, :authentication,
-    :confidentiality_impact_id, :integrity_impact_id, :availability_impact_id
-  has_one :confidentiality_impact
-  has_one :availability_impact
-  has_one :integrity_impact
+class FIDIUS::CveDb::Impact < FIDIUS::CveDb::Connection
+	attr_accessible :name
 end
