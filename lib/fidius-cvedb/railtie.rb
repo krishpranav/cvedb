1cab7378c26ebf5d0c8d8f0d06f615a1963a32f8
krishpranav <krisna.pranav@gmail.com>
2021-04-20 09:47:10 +0530

    railtie updated

---
+++ lib/fidius-cvedb/railtie.rb
@@ -0,0 +1,14 @@
+require 'fidius-cvedb'
+require 'rails'
+
+module FIDIUS
+	module CveDb
+		class Railtie < Rails::Railtie
+			rake_tasks do 
+				load "tasks/parser_cves.rake"
+				load "tasks/db_backup.rake"
+				load "tasks/nvd_migrate.rake"
+			end
+		end
+	end
+end
